19acf21f70d8c53a7471a5148135c936e98a402f
ashishbhungani97 <kanji201196@gmail.com>
2024-05-30 14:00:30 +0530

    Extra code remove

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,12 +4,8 @@ namespace App\Http\Controllers\Auth;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-use App\Models\User;
-use Illuminate\Support\Str;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-use kornrunner\Keccak;
 use Illuminate\Support\Facades\Auth;
-use Elliptic\EC;
 
 class LoginController extends Controller
 {
@@ -17,10 +13,6 @@ class LoginController extends Controller
     use AuthenticatesUsers;
 
     protected $redirectTo = '/';
-
-
-
-
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
@@ -44,81 +36,6 @@ class LoginController extends Controller
             return redirect()->route('admin')->with('success', 'Login successfully.');
         } else {
             return back()->withInput($request->input())->withErrors(['error_message' => 'Invalid credential.']);
-        }
-    }
-
-
-    public function authenticate(Request $request)
-    {
-        $ethAddress = Str::lower($request->ethAddress);
-        $message = $request->message;
-        $signature = $request->signature;
-
-        $valid = $this->verifySignature($message, $signature, $ethAddress);
-        if (!$valid) {
-            return response()->json(['message' => 'Invalid signature'], 401);
-        }
-
-        $user = User::where('eth_address', $ethAddress)->first();
-
-        if (!$user) {
-            return response()->json(['message' => 'User with address not exist , please signup first'], 400);
-        }
-
-        Auth::login($user);
-        return response()->json(['message' => 'Login successful'], 200);
-    }
-
-
-    protected function verifySignature($message, $signature, $address): bool
-    {
-        $messageLength = strlen($message);
-        $hash = Keccak::hash("\x19Ethereum Signed Message:\n{$messageLength}{$message}", 256);
-        $sign = [
-            "r" => substr($signature, 2, 64),
-            "s" => substr($signature, 66, 64)
-        ];
-
-        $recId = ord(hex2bin(substr($signature, 130, 2))) - 27;
-
-        if ($recId != ($recId & 1)) {
-            return false;
-        }
-
-        $publicKey = (new EC('secp256k1'))->recoverPubKey($hash, $sign, $recId);
-
-        return $this->pubKeyToAddress($publicKey) === Str::lower($address);
-    }
-
-    protected function pubKeyToAddress($publicKey): string
-    {
-        return "0x" . substr(Keccak::hash(substr(hex2bin($publicKey->encode("hex")), 1), 256), 24);
-    }
-
-
-    public function registerAddress(Request $request)
-    {
-        try {
-            $ethAddress = Str::lower($request->ethAddress);
-            $message = $request->message;
-            $signature = $request->signature;
-
-            $checkAddressNotExist = User::where('eth_address', $ethAddress)->get();
-            if ($checkAddressNotExist && count($checkAddressNotExist) > 0) {
-                return response()->json(['error' => 'Address already exist, please try again with different wallet address'], 200);
-            }
-
-            $valid = $this->verifySignature($message, $signature, $ethAddress);
-            if (!$valid) {
-                return response()->json(['error' => 'Invalid signature'], 200);
-            }
-
-            $user = Auth::user();
-            $user->update(['eth_address' => $ethAddress]);
-
-            return response()->json(['error' => 'OK'], 200);
-        } catch (Exception $e) {
-            return response()->json(['error' => $e->getMessage()], 200);
         }
     }
 
--- routes/web.php
+++ routes/web.php
@@ -4,49 +4,19 @@ use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminController;
 use App\Http\Controllers\Admin\CategoryController;
-use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\Auth\LoginController;
-use App\Mail\DynamicMail;
-use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Auth;
 
-Route::get('/', [HomeController::class, 'index'])->name('home');
-Auth::routes(['verify' => true]);
-
-Route::post('/authenticate', [App\Http\Controllers\Auth\LoginController::class, 'authenticate'])->name('authenticate');
-
-Route::get('/about', [App\Http\Controllers\HomeController::class, 'about'])->name('about');
-Route::get('/faq', [App\Http\Controllers\HomeController::class, 'faq'])->name('faq');
-Route::get('/public-profile/{id}', [App\Http\Controllers\HomeController::class, 'publicProfile']);
-
-Route::middleware(['auth', 'role_check:user', 'verified'])->group(function () {
-
-    Route::get('/account/statistics', [App\Http\Controllers\HomeController::class, 'accountStatistics'])->name('accountStatistics');
-    Route::get('/wallet/deposit', [App\Http\Controllers\HomeController::class, 'walletDeposit'])->name('wallet-deposit');
-    Route::get('/wallet/withdraw', [App\Http\Controllers\HomeController::class, 'walletWithdraw'])->name('wallet-withdraw');
-    Route::post('/wallet/withdraw', [App\Http\Controllers\HomeController::class, 'walletWithdrawStore'])->name('wallet-withdraw-store');
-
-    Route::get('/wallet/deposit/usdt', [App\Http\Controllers\HomeController::class, 'walletDepositUsdt'])->name('wallet-deposit-usdt');
-    Route::get('/wallet/withdraw/usdt', [App\Http\Controllers\HomeController::class, 'walletWithdrawUsdt'])->name('wallet-withdraw-usdt');
-    Route::post('/wallet/withdraw/usdt', [App\Http\Controllers\HomeController::class, 'walletWithdrawStoreUsdt'])->name('wallet-withdraw-store-usdt');
-
-});
-
+Route::get('/', [HomeController::class, 'index']);
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Auth::routes();
 
 // Admin
 Route::prefix('admin')->group(function () {
     Route::get('login', [LoginController::class, 'showAdminLoginForm']);
     Route::post('login', [LoginController::class, 'adminLogin'])->name('admin.login');
 });
-
 Route::get('/admin', [AdminController::class, 'index'])->name('admin')->middleware(['auth', 'role_check:admin']);
-
 Route::prefix('admin')->middleware(['auth', 'role_check:admin'])->group(function () {
     Route::resource('/category', CategoryController::class);
-});
-
-Route::get('/send-email', function () {
-    $data = ['name' => 'John Doe'];
-    $view = 'dynamic_mail.test';
-    Mail::to('recipient@example.com')->send(new DynamicMail($view, $data));
-    return "Email sent successfully!";
 });
